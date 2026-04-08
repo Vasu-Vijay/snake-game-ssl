3e15d294fbe08aa6fd00160152ad0b4d2e4eeca7
Yash Meshiya <meshiyayash@gmail.com>
2026-04-08 13:48:52 +0000

    feat: Added valid_command , tabular_display and Added Formating to existing features

--- admin.sh
+++ admin.sh
@@ -6,21 +6,114 @@ user=all
 #Different type of operations that can be performed from menu.
 operation=("Query_User" "Recent_Score" "Analytics" "Delete_Entries" "Log_Rotation" "Specific_View" "Exit")
 
-#Displays menu with selectable operations on terminal.
-function menu_display(){ 
-    printf "1] Query about a Specific User\n"
-    printf "2] View scores of recent games\n"
-    printf "3] View Analytics\n"
-    printf "4] Delete Entries\n"
-    printf "5] Log Rotation\n"
-    printf "6] Specific View\n"
-    printf "7] Exit\n"
+#To be used to validate if command entered is correct or not
+function valid_command(){
+    if [[ "$2" =~ ^[1-$1]$ || $2 == "q" || $2 == "\x1b" ]]; then 
+        return 0
+    else 
+        return -1
+    fi 
+}
 
-    read command
-    while ! [[ "$command" =~ ^[1-7]$ ]]
+#Used to display Menu in a tabluar form which is compatible with size of terminal
+function tabular_display(){
+    COLS=3 
+    PADDING=1
+    
+    COL_WIDTH=$(($(tput cols) / COLS - 1)) #$COLUMNS does not work as it needs to be run in terminal. $tput cols handles it as long as terminal is active
+    # Used to wrap text such that it stays in a block in menu
+    function wrap_text() {
+        fold -s -w $((COL_WIDTH - 2*PADDING)) <<<"$1"
+    }
+
+    #pints an entire row in the table
+    function print_row() {
+        local data_row=("$@")
+        local wrapped=() #stores wrapped text for each block
+        local maxlines_block=0 #max no.of lines used in a row to adjust height of column consistently in all blocks
+
+        for col in "${data_row[@]}"
+        do
+            lines=()
+                while read -r line; do
+                    lines+=("$line")
+                done < <(wrap_text "$col")
+            wrapped+=("$(printf "%s\n" "${lines[@]}")")
+            if [[ "${#lines[@]}" -gt "$maxlines_block" ]]; then
+                maxlines_block=${#lines[@]}
+            fi
+        done
+
+        for((y=0;y<COLS;y++)) #Prints separator between columns
+        do  
+            printf "|"
+            printf "%*s" $PADDING ""
+                printf "%-*s" $((COL_WIDTH - 2*PADDING)) 
+                printf "%*s" $PADDING ""
+        done
+        printf "|"
+        printf "\n"
+
+        for ((line=0; line<maxlines_block+1; line++))
+        do
+            printf "|"
+            for ((j=0; j<COLS; j++))
+            do
+                # Extract this block's lines
+                IFS=$'\n' read -d '' -r -a block_lines <<< "${wrapped[j]}" #-d is important as otherwise reading stops at even spaces
+                text="${block_lines[line]}"
+
+                # Padding + alignment
+                printf "%*s" $PADDING ""
+                printf "\e[35m%-*s\e[0m" $((COL_WIDTH - 2*PADDING)) "$text"
+                printf "%*s" $PADDING ""
+                printf "|"
+            done
+            printf "\n"
+        done
+    }
+    
+    #Prints the border of table
+    function print_border() {
+        printf "+"
+        for ((c=0; c<COLS; c++)); do
+            for ((w=0; w<COL_WIDTH; w++)); do
+                printf "-"
+            done
+            printf "+"
+        done
+        printf "\n"
+    }
+
+    #Prints the Table
+    for ((i=0; i<${#options[@]}; i+=COLS))
     do
-        printf "Please enter a valid command\n"
-        read command
+        print_border
+        row=()
+        for ((j=0; j<COLS; j++))
+        do
+            row+=("${options[i+j]}")
+        done
+        print_row "${row[@]}"
+        
+    done
+    print_border
+}
+
+
+#Displays menu with selectable operations on terminal.
+function menu_display(){
+    while true; do
+        options=("1] Query about a Specific User" "2] View scores of recent games" "3] View Analytics" "4] Delete Entries" "5] Log Rotation" "6] Specific View" "7] Exit")
+        tabular_display
+        read -p $'\e[33mEnter command : \e[0m' command #bash does not interpret escaping inside "" but understands it in $''
+
+        if valid_command ${#options[@]} $command; then
+            break
+        else 
+            printf "\033c" 
+            printf "\e[31mPlease enter a valid Command\e[0m\n"
+        fi
     done
     ${operation[$command - 1]}
 }
@@ -156,34 +249,46 @@ function Analytics(){
 #Delete Entries from history.txt
 function Delete_Entries(){
     printf "\033c"
-    printf "Choose a method to delete\n"
-    printf "1] Specific User\n"
-    printf "2] Timestamp\n"
-    printf "3] Misformatted Records\n"
-    read method
-    
-    if [[ $method == 1 ]] ; then #Delete Entries based on User 
-        read -p "Specify a User : " player 
-        read -p "Are you sure you want to delete these entries?(y/n)" confirmation
+    while true; do
+        options=("1] Specific User" "2] Timestamp" "3] Misformatted Records")
+        tabular_display
+        read -p $'\e[33mChoose a method to delete : \e[0m' method
+
+        if valid_command 3 $method; then
+            break
+        else 
+            printf "\033c" 
+            printf "\e[31mPlease enter a valid Command\e[0m\n"
+        fi
+    done
+
+    if [[ "$method" == $'\x1b' || "$method" == "q" ]] ;then #"\x1b" does not work as it is interpreted as string. So use $' ' to interpret backslash characters
+        menu_display
+    elif [[ $method == 1 ]] ; then 
+        read -p $'\e[33mSpecify a User : \e[0m' player
+        read -p $'\e[31mAre you sure you want to delete these entries ? (y/n) - \e[0m' confirmation
         if [[ $confirmation == "y" ]]; then
             awk -F "," -v user="$player" '
                 {
-                    if($2 != user)){
+                    if($2 != user){
                         print $0
                     }
                 }' history.txt > history.tmp
             
             mv history.tmp history.txt
-            printf "history.txt has been updated\n"
+            
+            
+            printf "\e[32mhistory.txt has been updated\e[0m\n"
+            update_userlist
         else 
             menu_display
         fi
-    elif [[ $method == 2 ]]; then #Delete entries Based on timestamp
-        read -p "Enter time stamp in format YYYY-MM-DD HH:MM:SS" timestamp
-        if date -d "$timestamp" "+%Y-%m-%d %H:%M:%S" >/dev/null 2>&1 ; then 
-            read -p "Do you want to delete entries after the timestamp or before (1/2)" option
+    elif [[ $method == 2 ]]; then
+        read -p $'\e[33mEnter time stamp in format ( YYYY-MM-DD HH:MM:SS ) - \e[0m' timestamp
+        if Validate_Timestamp $timestamp ; then 
+            read -p $'\e[33mDo you want to delete entries after the timestamp or before (1/2) - \e[0m' option
             if [[ $option == 1 ]]; then
-                read -p "Are you sure you want to delete these entries?(y/n)" confirmation
+                read -p $'\e[31mAre you sure you want to delete these entries ? (y/n) - \e[0m' confirmation
                 if [[ $confirmation == "y" ]]; then
                     awk -F "," -v timestamp="[$timestamp]" '
                         {
@@ -193,12 +298,15 @@ function Delete_Entries(){
                         }
                     ' history.txt > history.tmp
                     mv history.tmp history.txt
-                    printf "history.txt has been updated\n"
+                    
+                    printf "\e[32mhistory.txt has been updated\e[0m\n"
+                    update_userlist
+                    printf "\033c"
                 else 
                     menu_display
                 fi
             elif [[ $option == 2 ]]; then
-                read -p "Are you sure you want to delete these entries?(y/n)" confirmation
+                read -p $'\e[31mAre you sure you want to delete these entries ? (y/n) - \e[0m' confirmation
                 if [[ $confirmation == "y" ]]; then
                     awk -F "," -v timestamp="[$timestamp]" '
                         {
@@ -208,13 +316,19 @@ function Delete_Entries(){
                         }
                     ' history.txt >  history.tmp
                     mv history.tmp history.txt
-                    printf "history.txt has been updated\n"
+
+                    printf "\e[32mhistory.txt has been updated\e[0m\n"
+                    update_userlist
+                    printf "\033c"
                 else
                     menu_display
                 fi
             fi
+        else 
+            printf "\e[31mInvalid Timestamp\e[0m\n"
         fi
-    elif [[ $method == 3 ]]; then #Delete Entries if they do not match format
+    #Does not verify If date is correct or not 
+    elif [[ $method == 3 ]]; then
         awk -F "," '{
             if ($0 ~ /^\[[0-9]+-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\],[^,]+,[0-9]+,[A-Z]+,[0-9]+$/) {
                 if ($4 !~ /(WALL|SELF)/){}
